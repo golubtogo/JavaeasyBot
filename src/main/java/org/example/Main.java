a98387947f84164977caa15c642106bcde691cab
Nata <golub.togo@gmail.com>
2023-03-21 18:16:12 +0100

    Attach Buttons

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,18 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
-import org.telegram.telegrambots.meta.generics.LongPollingBot;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 public class Main extends TelegramLongPollingBot {
-    //JavaeasyBot
-    //6230171372:AAEdJc9tfhi_D45k9hth15xIapG9C0OAWQ0
+
     public static void main(String[] args) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(new Main());
@@ -31,6 +36,10 @@ public class Main extends TelegramLongPollingBot {
         Long chatId = getChatId(update);
 
         SendMessage msg = createMessage("*Hello* Nataliia");
+        attachButtons(msg, Map.of(
+                "BTN 1", "hello_btn_1",
+                "BTN 2", "hello_btn_2"
+        ));
         msg.setChatId(chatId);
         sendApiMethodAsync(msg);
 
@@ -50,5 +59,22 @@ public class Main extends TelegramLongPollingBot {
         message.setParseMode("markdown");
         return message;
 
+    }
+    public void attachButtons(SendMessage message, Map<String, String> buttons){
+        InlineKeyboardMarkup markup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+
+        for (String buttonName : buttons.keySet()){
+            String buttonValue = buttons.get(buttonName);
+
+            InlineKeyboardButton button = new InlineKeyboardButton();
+            button.setText(buttonName);
+            button.setCallbackData(buttonValue);
+
+            keyboard.add(Arrays.asList(button));
+        }
+
+        markup.setKeyboard(keyboard);
+        message.setReplyMarkup(markup);
     }
 }
